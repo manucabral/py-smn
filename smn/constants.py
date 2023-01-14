760887d38d3761fc1ced7b0613e5072bd84b63a9
Manu <mcabral@frba.utn.edu.ar>
2023-01-13 22:08:49 -0300

    feat(constants): add ip endpoint

--- smn/constants.py
+++ smn/constants.py
@@ -3,4 +3,5 @@ class SMNConstants:
     BASE_ENDPOINT = 'https://ws.smn.gob.ar'
     WEATHER_ENDPOINT = f'{BASE_ENDPOINT}/map_items/weather'
     FORECAST_ENDPOINT = f'{BASE_ENDPOINT}/map_items/forecast/{{days}}'
+    IP_API_ENDPOINT = 'http://ip-api.com/json'
     AVAILABLE_FORECASTS = ['now', '1 day', '2 days', '3 days', '4 days']
