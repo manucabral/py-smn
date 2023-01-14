411d7fdff9bc29e87ce2c3082a872fd52597a96c
Manu <mcabral@frba.utn.edu.ar>
2023-01-14 01:31:36 -0300

    feat: add exceptions

--- smn/client.py
+++ smn/client.py
@@ -5,6 +5,7 @@ import json
 from typing import List, Union, Tuple
 from .forecast import Forecast
 from .constants import SMNConstants
+from .exceptions import LimitExceeded, ForecastNotAvailable
 
 class Client:
     '''Client for the SMN API'''
@@ -13,22 +14,30 @@ class Client:
     def __init__(self, session: aiohttp.ClientSession = None):
         self.__session = session or aiohttp.ClientSession()
     
-    async def get(self, forecast: str = 'now', days: int = 1) -> Union[Forecast, List[Forecast]]:
+    async def get(self, forecast: str = 'now') -> Union[Forecast, List[Forecast]]:
         if forecast not in SMNConstants.AVAILABLE_FORECASTS:
-            # TODO: Raise a custom exception :p
-            raise ValueError(f'Forecast must be one of {self.__available_forecasts}')
-        endpoint = SMNConstants.WEATHER_ENDPOINT if forecast == 'now' else SMNConstants.FORECAST_ENDPOINT.format(days=days)
-        async with self.__session.get(endpoint) as response:
-            return Forecast(await response.json())
-    
+            raise ForecastNotAvailable(f'The forecast "{forecast}" is not available.')
+        if forecast == 'now':
+            endpoint = SMNConstants.WEATHER_ENDPOINT
+        else:
+            day = forecast.split(' ')[0]
+            endpoint = SMNConstants.FORECAST_ENDPOINT.format(days=day)
+        try:
+            async with self.__session.get(endpoint) as response:
+                return Forecast(await response.json())
+        except asyncio.TimeoutError:
+            raise LimitExceeded('Could not get forecast, exceeded the limit of requests.')
+        
     async def get_location(self) -> Tuple[str, float, float]:
-        async with self.__session.get(SMNConstants.IP_API_ENDPOINT) as response:
-            data = await response.json()
-            if response.status != 200:
-                # TODO: Raise a custom exception :p
-                raise ValueError('Could not get location, exceeded the limit of requests.')
-            return data['regionName'], float(data['lat']), float(data['lon'])
-
+        try:
+            async with self.__session.get(SMNConstants.IP_API_ENDPOINT) as response:
+                data = await response.json()
+                if response.status != 200:
+                    raise LimitExceeded('Could not get location, exceeded the limit of requests.')
+                return data['regionName'], float(data['lat']), float(data['lon'])
+        except asyncio.TimeoutError:
+            raise LimitExceeded('Could not get location, exceeded the limit of requests.')
+        
     async def __aenter__(self) -> 'Client':
         return self
     
--- smn/exceptions.py
+++ smn/exceptions.py
@@ -1,1 +1,4 @@
-# TODO
+class LimitExceeded(Exception):
+    """Raised when the limit of requests per minute is exceeded."""
+class ForecastNotAvailable(Exception):
+    """Raised when the forecast is not available for the given location."""
