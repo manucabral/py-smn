92ffd8c99d5810b04860ee8e86e2bd609319bd1c
Manu <mcabral@frba.utn.edu.ar>
2023-01-15 15:00:22 -0300

    feat(constants): remove unused endpoints

--- smn/constants.py
+++ smn/constants.py
@@ -2,7 +2,5 @@ class SMNConstants:
     '''Constants used by the SMN API'''
     API_ENDPOINT = 'https://ws.smn.gob.ar/map_items/'
     STATIC_ENDPOINT = f'https://ssl.smn.gob.ar/dpd/zipopendata.php?dato='
-    API_WEATHER_ENDPOINT = f'{API_ENDPOINT}/map_items/weather/'
-    API_FORECAST_ENDPOINT = f'{API_ENDPOINT}/map_items/forecast/{{days}}'
     API_IP_ENDPOINT = 'http://ip-api.com/json'
     AVAILABLE_FORECASTS = ['now', '1 day', '2 days', '3 days', '4 days']
