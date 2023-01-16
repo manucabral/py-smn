74261881228d6e54140de38f18d3d7ed72e21ca8
Manu <mcabral@frba.utn.edu.ar>
2023-01-15 23:14:38 -0300

    feat: add static example

--- example.py
+++ example.py
@@ -3,12 +3,10 @@ import smn
 
 async def main():
     async with smn.Client() as client:
-        forecast_now = await client.get(forecast='now')
+        forecast_now = await client.get_static()
         province, lat, lon = await client.get_location()
-        # province is not used in this example, but it's a good idea to use it!
-        nearest = forecast_now.nearest(lat, lon)
-        print(nearest.name)
-        print(nearest.weather['temp'])
+        nearest_forecast = forecast_now.nearest(lat, lon)
+        print(nearest_forecast.weather['temp'])
 
 if __name__ == '__main__':
     asyncio.run(main())
