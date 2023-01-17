77ef31c820b363a06795970a9b766d99babac5e8
Manu <mcabral@frba.utn.edu.ar>
2023-01-17 12:49:53 -0300

    feat: add setup for pip

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-smn',
+    version='0.0.3',
+    description='A Python wrapper for the SMN API Argentina',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/manucabral/py-smn',
+    project_urls={
+        'Documentation': 'https://github.com/manucabral/py-smn/blob/main/README.md',
+        'Bug Tracker': 'https://github.com/manucabral/py-smn/issues',
+    },
+    author='Manuel Cabral',
+    keywords=['smn', 'weather', 'forecast', 'api', 'argentina', 'smn-api'],
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['aiohttp'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
