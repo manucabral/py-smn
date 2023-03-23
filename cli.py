f5792a6d6ea5deb4222f9dcbf36e79a9894e0e3d
leoarayav <leandro.araya08@inacapmail.cl>
2023-03-23 00:39:55 -0300

    feat(cli): nearest positional command.

--- cli.py
+++ cli.py
@@ -16,19 +16,34 @@ async def get_province(province: str, name: str) -> None:
         for station in weather_stations:
             print(station.name, station.weather['temp'])
 
+async def get_nearest_weather() -> None:
+    async with Client() as client:
+        fcast_now = await client.get(forecast="now")
+        province, lat, lon = await client.get_location()
+        nearest_forecast = fcast_now.nearest(lat, lon)
+        temp = nearest_forecast.weather['temp']
+        station_name = nearest_forecast.name
+        print(f'The temperature in {station_name} is {temp}°C')
+
 def get_arguments() -> ArgumentParser:
+    '''
+        Get arguments from command line.
+
+        Returns:
+            ArgumentParser
+    '''
     parser = ArgumentParser(
         description='Non oficial @py-smn CLI client',
         epilog='Check main github repository for more information: https://github.com/manucabral/py-smn'
     )
-    """
-    Here goes arguments for the script to use
-    :author @leoarayav
-    """
     parser.add_argument("--province", help="Province to get data from")
     parser.add_argument("--name", help="Name of the city to get data from")
+    parser.add_argument("nearest", help="Get the nearest weather station")
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = get_arguments()
-    if args.province: run(get_province(args.province, args.name))
+    if args.province: 
+        run(get_province(args.province, args.name))
+    elif args.nearest: 
+        run(get_nearest_weather())
